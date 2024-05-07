4e7e93f6a6fdc897203fb965082c29c4b417ca84
Oshchypok <stefan.oshchypok@alumnos.upm.es>
2024-05-07 22:46:40 +0200

    feat: GestorErrores

--- src/main/java/upm/GestorErrores.java
+++ src/main/java/upm/GestorErrores.java
@@ -1,4 +1,25 @@
 package upm;
 
+import upm.cli.CommandLineInterface;
+import upm.cli.Vista;
+
 public class GestorErrores {
+    private final CommandLineInterface commandLineInterface;
+    private final Vista vista;
+
+    public GestorErrores(CommandLineInterface commandLineInterface, Vista vista) {
+        this.commandLineInterface = commandLineInterface;
+        this.vista = vista;
+    }
+
+    public void run() {
+        boolean exit = false;
+        while (!exit) {
+            try {
+
+            } catch (Exception e) {
+
+            }
+        }
+    }
 }
