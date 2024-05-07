c7d963e5b3e8fbe6f1f1c2e63f6adb5ce2c7065d
Oshchypok <stefan.oshchypok@alumnos.upm.es>
2024-05-07 22:46:10 +0200

    feat: adaptar App a InyectorDependencias

--- src/main/java/upm/App.java
+++ src/main/java/upm/App.java
@@ -2,6 +2,6 @@ package upm;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        InyectorDependencias.getInyectorDependencias().run();
     }
 }
