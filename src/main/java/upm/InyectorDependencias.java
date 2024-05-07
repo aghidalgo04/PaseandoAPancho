77dc43e0355432664e6393b3cfabd43d4a07663e
Oshchypok <stefan.oshchypok@alumnos.upm.es>
2024-05-07 22:45:38 +0200

    feat: completar InyectorDependencias

--- src/main/java/upm/InyectorDependencias.java
+++ src/main/java/upm/InyectorDependencias.java
@@ -1,4 +1,84 @@
 package upm;
 
+import upm.cli.CommandLineInterface;
+import upm.cli.Vista;
+import upm.cli.VistaConsola;
+import upm.controlador.ControladorMascota;
+import upm.controlador.ControladorUsuario;
+import upm.data.modelo.Usuario;
+import upm.data.persitencia.PersistenciaContratoCuidado;
+import upm.data.persitencia.PersistenciaMascota;
+import upm.data.persitencia.PersistenciaUsuario;
+import upm.data.persitencia.map.PersistenciaContratoCuidadoMap;
+import upm.data.persitencia.map.PersistenciaMap;
+import upm.data.persitencia.map.PersistenciaMascotaMap;
+
 public class InyectorDependencias {
+    private static final InyectorDependencias inyectorDependencias = new InyectorDependencias();
+
+    private final PersistenciaUsuario persistenciaUsuario;
+    private final PersistenciaMascota persistenciaMascota;
+    private final PersistenciaContratoCuidado persistenciaContratoCuidado;
+
+    private final ControladorUsuario controladorUsuario;
+    private final ControladorMascota controladorMascota;
+
+    private final CommandLineInterface commandLineInterface;
+    private final Vista vista;
+
+    private final GestorErrores gestorErrores;
+
+    private InyectorDependencias() {
+        this.persistenciaUsuario = new PersistenciaMap<Usuario>();
+        this.persistenciaMascota = new PersistenciaMascotaMap();
+        this.persistenciaContratoCuidado = new PersistenciaContratoCuidadoMap();
+
+        this.controladorUsuario = new ControladorUsuario(this.persistenciaUsuario, this.persistenciaMascota, this.persistenciaContratoCuidado);
+        this.controladorMascota = new ControladorMascota(this.persistenciaMascota);
+
+        this.commandLineInterface = new CommandLineInterface();
+        this.vista = new VistaConsola();
+
+        this.gestorErrores = new GestorErrores(this.commandLineInterface, this.vista);
+    }
+
+    public static InyectorDependencias getInyectorDependencias() {
+        return inyectorDependencias;
+    }
+
+    public void run() {
+        this.gestorErrores.run();
+    }
+
+    public PersistenciaUsuario getPersistenciaUsuario() {
+        return this.persistenciaUsuario;
+    }
+
+    public PersistenciaMascota getPersistenciaMascota() {
+        return this.persistenciaMascota;
+    }
+
+    public PersistenciaContratoCuidado getPersistenciaContratoCuidado() {
+        return this.persistenciaContratoCuidado;
+    }
+
+    public ControladorUsuario getControladorUsuario() {
+        return this.controladorUsuario;
+    }
+
+    public ControladorMascota getControladorMascota() {
+        return this.controladorMascota;
+    }
+
+    public CommandLineInterface getCommandLineInterface() {
+        return this.commandLineInterface;
+    }
+
+    public Vista getVista() {
+        return this.vista;
+    }
+
+    public GestorErrores getGestorErrores() {
+        return this.gestorErrores;
+    }
 }
