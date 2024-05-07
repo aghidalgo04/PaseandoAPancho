518f2ef76b8f9089507a3d05b0622c15e55bd216
Oshchypok <stefan.oshchypok@alumnos.upm.es>
2024-05-07 23:16:20 +0200

    feat: leerComando de VistaConsola

--- src/main/java/upm/cli/VistaConsola.java
+++ src/main/java/upm/cli/VistaConsola.java
@@ -1,14 +1,69 @@
 package upm.cli;
 
+import java.util.List;
 import java.util.Scanner;
 
-public class VistaConsola implements Vista{
+public class VistaConsola implements Vista {
     private static final String DELIMITER_COLON_OR_RETURN = "[:,\\r\\n]";
 
     private final Scanner scanner;
 
-    public VistaConsola(){
+    public VistaConsola() {
         this.scanner = new Scanner(System.in).useDelimiter(DELIMITER_COLON_OR_RETURN);
     }
 
+    @Override
+    public String[] leerComando() {
+        String comando = this.scanner.next().trim().toLowerCase();
+        String[] argumentos = leerArgumentos();
+        if (argumentos != null) {
+            String[] input = new String[1+argumentos.length];
+            input[0] = comando;
+            System.arraycopy(argumentos, 0, input, 1, argumentos.length);
+            return input;
+        } else {
+            return new String[]{comando};
+        }
+    }
+
+    private String[] leerArgumentos() {
+        String input = this.scanner.nextLine().trim();
+        if (!input.isEmpty()) {
+            return filterArgumentosSpace(input.replaceFirst(":", "").split(";"));
+        } else {
+            return null;
+        }
+    }
+
+    private String[] filterArgumentosSpace(String[] valores) {
+        for (int i = 0; i < valores.length; i++) {
+            valores[i] = valores[i].trim();
+        }
+        return valores;
+    }
+
+    @Override
+    public void mostrarComando(String comando) {
+
+    }
+
+    @Override
+    public void mostrarComandosDisponibles(List<String[]> comandos) {
+
+    }
+
+    @Override
+    public void mostrarError(String error) {
+
+    }
+
+    @Override
+    public void mostrarUsuario(Long id, String nombre, String apellidos, String correoElectronico) {
+
+    }
+
+    @Override
+    public void mostrarMascota(Long id, String nombre, String direccion, String descripcion) {
+
+    }
 }
