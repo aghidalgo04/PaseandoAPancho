a98afcf1f358ca0ada2c6f71a2ca70dced8d019c
Stefan Oshchypok <stefan.oshchypok@alumnos.upm.es>
2024-05-12 12:52:29 +0200

    refactor: adaptar ControladorUsuario a Usuario

--- src/main/java/upm/controlador/ControladorUsuario.java
+++ src/main/java/upm/controlador/ControladorUsuario.java
@@ -13,13 +13,11 @@ import java.io.File;
 import java.util.List;
 
 public class ControladorUsuario {
-    private Long IDS;
     private PersistenciaUsuario persistenciaUsuario;
     private PersistenciaMascota persistenciaMascota;
     private PersistenciaContratoCuidado persistenciaContratoCuidado;
 
     public ControladorUsuario(PersistenciaUsuario persistenciaUsuario, PersistenciaMascota persistenciaMascota, PersistenciaContratoCuidado persistenciaContratoCuidado) {
-        this.IDS = 0L;
         this.persistenciaUsuario = persistenciaUsuario;
         this.persistenciaMascota = persistenciaMascota;
         this.persistenciaContratoCuidado = persistenciaContratoCuidado;
@@ -27,19 +25,19 @@ public class ControladorUsuario {
 
     public void regsitrarUsuarioComoDueno(String nombre, String apellidos, String correoElectronico, String direccion, Idioma idioma, Plataforma plataformaRegistro) {
         String idUsuario = ExternalRRSS.LoginRRSS();
-        Dueno dueno = new Dueno(++this.IDS, nombre, apellidos, correoElectronico, direccion, idioma, plataformaRegistro);
+        Dueno dueno = new Dueno(idUsuario, nombre, apellidos, correoElectronico, direccion, idioma, plataformaRegistro);
         this.persistenciaUsuario.createDueno(dueno);
     }
 
     public void regsitrarUsuarioComoCuidador(String nombre, String apellidos, String correoElectronico, String direccion, Idioma idioma, Plataforma plataformaRegistro, File foto, String descripcion, Integer precio, String IBAN) {
         String idUsuario = ExternalRRSS.LoginRRSS();
-        Cuidador cuidador = new Cuidador(++this.IDS, nombre, apellidos, correoElectronico, direccion, idioma, plataformaRegistro, foto, descripcion, precio, IBAN);
+        Cuidador cuidador = new Cuidador(idUsuario, nombre, apellidos, correoElectronico, direccion, idioma, plataformaRegistro, foto, descripcion, precio, IBAN);
         this.persistenciaUsuario.createCuidador(cuidador);
     }
 
     public void regsitrarUsuarioComoCuidadorConDocs(String nombre, String apellidos, String correoElectronico, String direccion, Idioma idioma, Plataforma plataformaRegistro, File foto, String descripcion, Integer precio, String IBAN, List<File> documentacion) {
         String idUsuario = ExternalRRSS.LoginRRSS();
-        Cuidador cuidador = new Cuidador(++this.IDS, nombre, apellidos, correoElectronico, direccion, idioma, plataformaRegistro, foto, descripcion, precio, IBAN, documentacion);
+        Cuidador cuidador = new Cuidador(idUsuario, nombre, apellidos, correoElectronico, direccion, idioma, plataformaRegistro, foto, descripcion, precio, IBAN, documentacion);
         this.persistenciaUsuario.createCuidador(cuidador);
     }
 
@@ -48,7 +46,7 @@ public class ControladorUsuario {
         // this.persistenciaUsuario.findDueno(idUsuario)
     }
 
-    public void anadirMascota(Long idDueno, Long idMascota) {
+    public void anadirMascota(String idDueno, Long idMascota) {
         this.persistenciaUsuario.findDueno(idDueno).anadirMascota(this.persistenciaMascota.findById(idMascota));
     }
 
