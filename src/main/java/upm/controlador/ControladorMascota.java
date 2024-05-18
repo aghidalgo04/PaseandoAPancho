0bec106878ecc174b0cf2e84e4ed9c79510dbad3
Stefan Oshchypok <stefan.oshchypok@alumnos.upm.es>
2024-05-18 10:49:10 +0200

    refactor: mover comprobacion de codigo RIAC a controlador

--- src/main/java/upm/controlador/ControladorMascota.java
+++ src/main/java/upm/controlador/ControladorMascota.java
@@ -1,5 +1,6 @@
 package upm.controlador;
 
+import servidor.ExternalRIAC;
 import upm.data.modelo.Album;
 import upm.data.modelo.Foto;
 import upm.data.modelo.Mascota;
@@ -20,11 +21,17 @@ public class ControladorMascota {
 
     public void crearMascota(String nombre, String direccion, String descripcion, String codigoRIAC, String polizaSeguro, List<Album> albums, Foto fotoFavorita) {
         this.IDS++;
+        if (!ExternalRIAC.RIAC(codigoRIAC)) {
+            throw new RuntimeException("Codigo RIAC no valido");    // @TODO habra que cambiar por excepciones personales
+        }
         Mascota mascota = new Mascota(this.IDS, nombre, direccion, descripcion, codigoRIAC, polizaSeguro, albums, fotoFavorita);
     }
 
     public void crearMascotaExotica(String nombre, String direccion, String descripcion, String codigoRIAC, String polizaSeguro, List<Album> albums, Foto fotoFavorita, File certificadoLegal, File certificadoSalud, File libreEnfermedadesTransmisibles) {
         this.IDS++;
+        if (!ExternalRIAC.RIAC(codigoRIAC)) {
+            throw new RuntimeException("Codigo RIAC no valido");    // @TODO habra que cambiar por excepciones personales
+        }
         Mascota mascota = new MascotaExotica(this.IDS, nombre, direccion, descripcion, codigoRIAC, polizaSeguro, albums, fotoFavorita, certificadoLegal, certificadoSalud, libreEnfermedadesTransmisibles);
     }
 
