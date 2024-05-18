e0aa7b594b99b3ffe4ffc0db3a2466aaebcf38ab
Stefan Oshchypok <stefan.oshchypok@alumnos.upm.es>
2024-05-18 11:16:50 +0200

    refactor: return finds por Optional

--- src/main/java/upm/data/persitencia/PersistenciaMascota.java
+++ src/main/java/upm/data/persitencia/PersistenciaMascota.java
@@ -2,8 +2,5 @@ package upm.data.persitencia;
 
 import upm.data.modelo.Mascota;
 
-import java.util.List;
-
 public interface PersistenciaMascota extends Persistencia<Mascota> {
-    List<Mascota> findAll();
 }
--- src/main/java/upm/data/persitencia/map/PersistenciaUsuarioMap.java
+++ src/main/java/upm/data/persitencia/map/PersistenciaUsuarioMap.java
@@ -2,13 +2,12 @@ package upm.data.persitencia.map;
 
 import upm.data.modelo.Cuidador;
 import upm.data.modelo.Dueno;
-import upm.data.modelo.Usuario;
 import upm.data.persitencia.PersistenciaUsuario;
 
 import javax.swing.text.html.parser.Entity;
 import java.io.File;
-import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.TreeMap;
 
 public class PersistenciaUsuarioMap  implements PersistenciaUsuario {
@@ -33,12 +32,12 @@ public class PersistenciaUsuarioMap  implements PersistenciaUsuario {
     }
 
     @Override
-    public Dueno findDueno(String id) {
+    public Optional<Dueno> findDueno(String id) {
         return null;
     }
 
     @Override
-    public Cuidador findCuidador(String id) {
+    public Optional<Cuidador> findCuidador(String id) {
         return null;
     }
 
