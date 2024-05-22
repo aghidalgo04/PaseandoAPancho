16aa329046f798254c473c64c9c35c0ca27b89ea
DLopezRuiz <david.lopruiz@alumnos.upm.es>
2024-05-22 17:58:06 +0200

    Creacion start de test

---
+++ src/test/java/upm/controlador/ControladorUsuarioTest.java
@@ -0,0 +1,19 @@
+package upm.controlador;
+
+import org.junit.Test;
+import upm.InyectorDependencias;
+
+public class ControladorUsuarioTest {
+    private final ControladorUsuario controladorUsuario = InyectorDependencias.getInyectorDependencias().getControladorUsuario();
+
+    @Test
+    public void testRegistrarDueno(){
+
+    }
+
+    @Test
+    public void testRegistrarCuidador(){
+
+    }
+
+}
