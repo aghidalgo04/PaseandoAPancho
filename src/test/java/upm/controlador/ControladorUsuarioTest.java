6468aabc5dd2c86fa5bb099950ac8f8c87d0c6b1
DLopezRuiz <david.lopruiz@alumnos.upm.es>
2024-05-23 16:24:21 +0200

    test login creado

--- src/test/java/upm/controlador/ControladorUsuarioTest.java
+++ src/test/java/upm/controlador/ControladorUsuarioTest.java
@@ -13,6 +13,7 @@ import static org.junit.Assert.assertEquals;
 public class ControladorUsuarioTest {
     private final ControladorUsuario controladorUsuario = InyectorDependencias.getInyectorDependencias().getControladorUsuario();
     private final PersistenciaUsuario persistenciaUsuario = InyectorDependencias.getInyectorDependencias().getPersistenciaUsuario();
+    private final Session session = InyectorDependencias.getInyectorDependencias().getSession();
 
     @Test
     public void testRegistrarDueno(){
@@ -26,5 +27,17 @@ public class ControladorUsuarioTest {
         assertEquals(persistenciaUsuario.findCuidador("a8a9aff5f55133002b55c1682730344b").get().getId(),"a8a9aff5f55133002b55c1682730344b");
 
     }
+    @Test
+    public void testLogin(){
+        controladorUsuario.registrarCuidador("david","Lopez","a@upm.es","si", Idioma.Castellano, Plataforma.Google,null,"soy ese",15,"4564145165",null);
+        controladorUsuario.login();
+        assertEquals(session.getUsuario().getId(),"a8a9aff5f55133002b55c1682730344b");
+    }
+
+    @Test
+    public void testAniadirMascota(){}
+
+    @Test
+    public void testContratarCuidador(){}
 
 }
