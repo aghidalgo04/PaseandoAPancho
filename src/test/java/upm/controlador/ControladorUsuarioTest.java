c984f3c2325388a3c8e04e929596c29a335daaf6
DLopezRuiz <david.lopruiz@alumnos.upm.es>
2024-05-24 14:19:15 +0200

    Test aniadirMascota beta

--- src/test/java/upm/controlador/ControladorUsuarioTest.java
+++ src/test/java/upm/controlador/ControladorUsuarioTest.java
@@ -1,18 +1,23 @@
 package upm.controlador;
 
 import org.junit.Test;
+import servidor.ExternalRIAC;
 import servidor.ExternalRRSS;
 import upm.InyectorDependencias;
 import upm.data.modelo.Dueno;
 import upm.data.modelo.enums.Idioma;
 import upm.data.modelo.enums.Plataforma;
+import upm.data.persitencia.PersistenciaMascota;
 import upm.data.persitencia.PersistenciaUsuario;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class ControladorUsuarioTest {
     private final ControladorUsuario controladorUsuario = InyectorDependencias.getInyectorDependencias().getControladorUsuario();
+    private final ControladorMascota controladorMascota = InyectorDependencias.getInyectorDependencias().getControladorMascota();
     private final PersistenciaUsuario persistenciaUsuario = InyectorDependencias.getInyectorDependencias().getPersistenciaUsuario();
+    private final PersistenciaMascota persistenciaMascota = InyectorDependencias.getInyectorDependencias().getPersistenciaMascota();
     private final Session session = InyectorDependencias.getInyectorDependencias().getSession();
 
     @Test
@@ -35,7 +40,14 @@ public class ControladorUsuarioTest {
     }
 
     @Test
-    public void testAniadirMascota(){}
+    public void testAniadirMascota(){
+        controladorUsuario.registrarDueno("david","Lopez","a@upm.es","si", Idioma.Castellano, Plataforma.Google);
+        controladorUsuario.login();
+        controladorMascota.crearMascota("Ely","sierra","Perro blanco", "0123456789","213u84871",null,null);
+        assertNotNull(this.persistenciaMascota.findById(2L));
+        //controladorUsuario.anadirMascota(2L);
+        //assertEquals(persistenciaUsuario.findDueno("a8a9aff5f55133002b55c1682730344b").get().buscarMascota(2L),persistenciaMascota.findById(2L));
+    }
 
     @Test
     public void testContratarCuidador(){}
