101909eed538c2b23a7407fd96f49ee07f83fecc
alejandrogh <a.ghidalgo@alumnos.upm.es>
2024-05-27 18:38:05 +0200

    Hecho que files sean obligatorios

--- src/main/java/upm/cli/comandos/comandos/CrearMascotaExotica.java
+++ src/main/java/upm/cli/comandos/comandos/CrearMascotaExotica.java
@@ -5,11 +5,14 @@ import upm.cli.comandos.Comando;
 import upm.cli.excepciones.UnsupportedAttributesException;
 import upm.controlador.ControladorMascota;
 import upm.controlador.ControladorUsuario;
+import upm.data.modelo.excepciones.InvalidAttributeException;
+
+import java.io.File;
 
 public class CrearMascotaExotica implements Comando {
-    private static final String VALOR = "crear-mascota-exotica";
-    private static final String AYUDA_PARAMETROS = "<nombre>; <direccion>; <descripcion>; <codigoRIAC>; <polizaSeguro>; <albums>; <fotoFavorita>; <certificadoLegal>; <certificadoSalud>; <libreEnfermedadesTransmisibles>";
-    private static final String AYUDA_COMENTARIO = "Creas un dueno";
+    private static final String VALOR = "aniadir-mascota-exotica";
+    private static final String AYUDA_PARAMETROS = "<nombre>; <direccion>; <descripcion>; <codigoRIAC>; <certificadoLegal>; <certificadoSalud>; <libreEnfermedadesTransmisibles>; <polizaSeguro>; <albums>; <fotoFavorita>";
+    private static final String AYUDA_COMENTARIO = "Aniades una mascota exótica";
 
     private static final byte NUMERO_PARAMETROS = 10;
 
@@ -26,7 +29,10 @@ public class CrearMascotaExotica implements Comando {
         if (parametros.length != NUMERO_PARAMETROS) {
             throw new UnsupportedAttributesException(this.ayudaParametros());
         }
-        this.controladorUsuario.anadirMascota(this.controladorMascota.crearMascotaExotica(parametros[0], parametros[1], parametros[2], parametros[3], parametros[4], null, null, null, null, null));
+        if((!parametros[4].contains(".")) || (!parametros[5].contains(".")) || !parametros[6].contains(".")){
+            throw new InvalidAttributeException("No has introducido bien todos los certificados");
+        }
+        this.controladorUsuario.anadirMascota(this.controladorMascota.crearMascotaExotica(parametros[0], parametros[1], parametros[2], parametros[3], new File(parametros[4]), new File(parametros[5]), new File(parametros[6]), parametros[7], null, null));
         vista.mostarMensaje("Mascota exotica creada con exito");
     }
 
